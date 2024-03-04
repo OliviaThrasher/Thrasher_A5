d96788c1d47bc68483c9002f2a77a2c9b92f6e1b
Olivia Thrasher <156224902+OliviaThrasher@users.noreply.github.com>
2024-03-03 22:36:56 -0500

    Updated Form1.vb

--- Thrasher_A5/Form1.vb
+++ Thrasher_A5/Form1.vb
@@ -1,12 +1,17 @@
-﻿Public Class Form1
+'Olivia Thrasher
+'Assignment A5
+'GUI Development Class
+Public Class Form1
+    'decGroup is here so it can be a global variable
     Dim decGroup As Double
+    'When the form loads, it clears everything
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         TextCost.Clear()
         TextGroupSize.Clear()
         Radio1Super.Select()
         TextGroupSize.Focus()
     End Sub
-
+    '1 If statement for white space and 3 If statements for Less than 1 and more than 20 errors
     Private Sub TextGroupSize_TextChanged(sender As Object, e As EventArgs) Handles TextGroupSize.TextChanged
         Dim strGroup As String = TextGroupSize.Text
         If Not String.IsNullOrWhiteSpace(TextGroupSize.Text) Then
@@ -33,7 +38,7 @@
 
 
     End Sub
-
+    'This makes the caluclation based on which button is picked.
     Private Sub ButtonCalculate_Click(sender As Object, e As EventArgs) Handles ButtonCalculate.Click
         Dim ConSuper As Double = 380
         Dim ConAuto As Double = 275
@@ -58,7 +63,7 @@
         End If
 
     End Sub
-
+    'Clears Everything
     Private Sub ButtonClear_Click(sender As Object, e As EventArgs) Handles ButtonClear.Click
         TextCost.Clear()
         TextGroupSize.Clear()
